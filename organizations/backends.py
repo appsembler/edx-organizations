6fa6727ca4c1b162847b825d8a69c2fa120b18f5
Filip Jukić <filip@jukic.me>
2017-01-06 19:15:56 +0100

    don’t do microsite permission checking for default site

--- organizations/backends.py
+++ organizations/backends.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.backends import ModelBackend
 from django.core.exceptions import PermissionDenied
-
+from openedx.core.djangoapps.theming.helpers import get_current_site
 from openedx.core.djangoapps.site_configuration.helpers import is_site_configuration_enabled, get_value
 
 
@@ -13,7 +14,9 @@ class OrganizationMemberBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         user = super(OrganizationMemberBackend, self).authenticate(username, password, **kwargs)
         # superuser can log into any microsite
-        if is_site_configuration_enabled() and user and not user.is_superuser:
+        site = get_current_site()
+        is_default_site = site.id == settings.SITE_ID
+        if not is_default_site and is_site_configuration_enabled() and user and not user.is_superuser:
             user_organizations = set(user.organizations.values_list('short_name', flat=True))
             site_organization = get_value('course_org_filter')
             if site_organization not in user_organizations:

0f35939c95c4b7b19eacbbb79ebfb2e1dfa0608f
TJ Keemon <keems21@gmail.com>
2017-10-18 13:30:09 +0800

    only allow user-org backend login if mapping is_active=True

--- organizations/backends.py
+++ organizations/backends.py
@@ -4,6 +4,7 @@ from django.core.exceptions import PermissionDenied
 from openedx.core.djangoapps.theming.helpers import get_current_site
 from openedx.core.djangoapps.site_configuration.helpers import is_site_configuration_enabled, get_value
 
+from .models import UserOrganizationMapping
 
 class DefaultSiteBackend(ModelBackend):
     """
@@ -37,6 +38,15 @@ class OrganizationMemberBackend(ModelBackend):
         if not is_default_site and is_site_configuration_enabled() and user and not user.is_superuser:
             user_organizations = set(user.organizations.values_list('short_name', flat=True))
             site_organization = get_value('course_org_filter')
+
+            #make sure user has been granted permission to log into microsite
+            mapping = UserOrganizationMapping.objects.filter(
+                    organization__short_name=site_organization,
+                    user=user
+                ) 
+            if not mapping or not mapping.first().is_active:
+                return None
+
             if site_organization in user_organizations:
                 return user
         return None

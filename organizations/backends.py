58cb7e67033b48e2b792e9bf219ab63b8ceb43c0
Bryan Wilson <bryanlandia@gmail.com>
2020-12-31 22:52:45 +0000

    Allow multiple orgs per site for OrganizationMemberBackend if feature enabled

--- organizations/backends.py
+++ organizations/backends.py
@@ -1,8 +1,17 @@
+import collections
+import logging
+
 from django.conf import settings
 from django.contrib.auth.backends import ModelBackend
 from django.core.exceptions import PermissionDenied
-from openedx.core.djangoapps.theming.helpers import get_current_site
-from openedx.core.djangoapps.site_configuration.helpers import is_site_configuration_enabled, get_value
+
+from .test_compat import compat
+
+# from openedx.core.djangoapps.theming.helpers import get_current_site
+# from openedx.core.djangoapps.site_configuration.helpers import is_site_configuration_enabled, get_value
+
+
+logger = logging.getLogger(__name__)
 
 
 class DefaultSiteBackend(ModelBackend):
@@ -13,7 +22,7 @@ class DefaultSiteBackend(ModelBackend):
     def authenticate(self, *args, **kwargs):
         user = super(DefaultSiteBackend, self).authenticate(*args, **kwargs)
         # superuser can log into any microsite
-        site = get_current_site()
+        site = compat.get_current_site()
         is_default_site = site.id == settings.SITE_ID
         if user:
             if is_default_site:
@@ -32,19 +41,33 @@ class OrganizationMemberBackend(ModelBackend):
     def authenticate(self, *args, **kwargs):
         user = super(OrganizationMemberBackend, self).authenticate(*args, **kwargs)
         # superuser can log into any microsite
-        site = get_current_site()
+        site = compat.get_current_site()
         is_default_site = site.id == settings.SITE_ID
-        if not is_default_site and is_site_configuration_enabled() and user and not user.is_superuser:
+        if not is_default_site and compat.is_site_configuration_enabled() and user and not user.is_superuser:
             user_organizations = set(user.organizations.values_list('short_name', flat=True))
-            site_organization = get_value('course_org_filter')
+            site_organization = compat.get_value('course_org_filter')
+
+            # get a sequence and remove dupes
+            if not isinstance(site_organization, (tuple, collections.MutableSequence)):
+                site_organization = (site_organization, )
+            site_organization = set(site_organization)
+
             try:
-                if site_organization in user_organizations:
+                assert(len(site_organization) == 1)
+            except AssertionError:
+                if not settings.FEATURES.get('TAHOE_ALLOW_MULTIPLE_COURSE_ORGS_PER_ORGANIZATION', False):
+                    logger.warn(
+                        "Site {} configured with multiple course orgs but "
+                        "TAHOE_ALLOW_MULTIPLE_COURSE_ORGS_PER_ORGANIZATION "
+                        "not enabled.  Authentication failed.".format(site.domain)
+                    )
+                    return None
+
+            for org in site_organization:
+                if org in user_organizations:
                     return user
-            except TypeError:
-                # standalone supports multiple course orgs per Site
-                for org in site_organization:
-                    if org in user_organizations:
-                        return user
+            else:
+                return user
         return None
 
 
@@ -56,9 +79,9 @@ class SiteMemberBackend(ModelBackend):
     def authenticate(self, *args, **kwargs):
         user = super(SiteMemberBackend, self).authenticate(*args, **kwargs)
         # superuser can log into any microsite
-        site = get_current_site()
+        site = compat.get_current_site()
         is_default_site = site.id == settings.SITE_ID
-        if not is_default_site and is_site_configuration_enabled() and user and not user.is_superuser:
+        if not is_default_site and compat.is_site_configuration_enabled() and user and not user.is_superuser:
             if user.id in site.usersitemapping_set.select_related('user').values_list('user__id',flat=True):
                 return user
         return None
